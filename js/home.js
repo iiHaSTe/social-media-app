2e73f9b2eb9249a7c9073f2ac687b424c67c4578
www.hicham2005@gmail.com <www.hicham2005@gmail.com>
2022-05-25 12:38:31 +0100

    jdhfhc

--- js/home.js
+++ js/home.js
@@ -7,16 +7,36 @@ const shares = document.querySelectorAll("#share");
 
 function add(){
   let span = this.childNodes[3];
-  if (this.classList.toggle("selected"))
+  if (this.classList.toggle("selected")){
     span.innerText = parseInt(span.innerText) + 1;
-  else
+  }else
     span.innerText = parseInt(span.innerText) - 1;
 }
-likes.forEach(like => {
-  like.onclick = add;
+likes.forEach((like, i) => {
+  like.onclick = function() {
+    let span = this.childNodes[3];
+    if (this.classList.toggle("selected")) {
+      span.innerText = parseInt(span.innerText) + 1;
+    } else
+      span.innerText = parseInt(span.innerText) - 1;
+    if (dislikes[i].classList.contains("selected")) {
+      dislikes[i].classList.remove("selected");
+      dislikes[i].childNodes[3].innerText = parseInt(dislikes[i].childNodes[3].innerText) - 1;
+    }
+  }
 });
-dislikes.forEach(dislike => {
-  dislike.onclick = add;
+dislikes.forEach((dislike, i) => {
+  dislike.onclick = function (){
+    let span = this.childNodes[3];
+    if (this.classList.toggle("selected")) {
+      span.innerText = parseInt(span.innerText) + 1;
+    } else
+      span.innerText = parseInt(span.innerText) - 1;
+    if (likes[i].classList.contains("selected")){
+      likes[i].classList.remove("selected");
+      likes[i].childNodes[3].innerText = parseInt(likes[i].childNodes[3].innerText) - 1;
+    }
+  }
 });
 shares.forEach(share=>{
   share.onclick = add;
